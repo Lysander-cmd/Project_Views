98d9a8234592c9ac201ed3508d2b2e942d140477
Lysander-cmd <yafiqarza@gmail.com>
2022-03-04 10:07:58 +0700

    Project Views

---
+++ app/src/main/java/www/smktelkommalang/sch/myviewandviews/MainActivity.java
@@ -0,0 +1,18 @@
+package www.smktelkommalang.sch.myviewandviews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Google Pixel");}
+
+    }
+}
